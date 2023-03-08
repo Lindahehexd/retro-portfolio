9410e93c6551d7c7bf8b4670fe06a42045c9ae30
Lindahehexd <tkd09152129@gmail.com>
2023-03-08 16:48:01 +0800

    reupload the pdf file

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,5 @@
 import AboutMeBox from "@/components/AboutPage/AboutMeBox";
 import ContentLayout from "@/components/Layout/ContentLayout";
-import { Flex } from "@chakra-ui/react";
 
 export default function ContactPage() {
   return (
