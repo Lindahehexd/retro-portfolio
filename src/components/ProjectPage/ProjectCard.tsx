958adfde8153b8c4b8a507c440d17e11e7572ee8
lindahehexd <tkd09152129@gmail.com>
2023-03-12 14:53:30 +0800

    add shopping cart project and adjust the fontweight

--- src/components/ProjectPage/ProjectCard.tsx
+++ src/components/ProjectPage/ProjectCard.tsx
@@ -30,7 +30,7 @@ const ProjectCard = () => {
             </Flex>
             {/* body */}
             <CardBody>
-              <Flex direction="column" >
+              <Flex direction="column">
                 <Text fontSize="4xl" fontWeight={800} mb={2}>
                   PTTit{" "}
                 </Text>
@@ -117,7 +117,7 @@ const ProjectCard = () => {
                 </SimpleGrid>
               </Flex>
 
-              <Text fontSize="lg" fontWeight={700}>
+              <Text fontSize="lg" fontWeight={600}>
                 {" "}
                 結合PTT與Reddit,
                 使用者可以瀏覽看板、建立、加入、搜尋看板。並且可以發文、推文、噓文...等等。如果您是板主，甚至能客製化您的看板，包含圖片、看板簡介、看板標題。用戶也可對貼文點選喜歡或不喜歡，票數會即時顯示。此作品主要練習SSR、Type型別熟悉、使用狀態管理工具Recoil、擴展運算子、使用firebase達成CRUD...等等。{" "}
@@ -157,7 +157,7 @@ const ProjectCard = () => {
                 <Text fontSize="4xl" fontWeight={800} mb={2}>
                   聊天GO
                 </Text>
-                <SimpleGrid columns={{ base: 3, md: 3, lg: 5 }} spacing={2}  mb={5}>
+                <SimpleGrid columns={{ base: 3, md: 3, lg: 5 }} spacing={2} mb={5}>
                   <Flex
                     h="22px"
                     p={2}
@@ -175,7 +175,7 @@ const ProjectCard = () => {
                   </Flex>
 
                   <Flex
-                      h="22px"
+                    h="22px"
                     p={2}
                     boxShadow="4px 4px black"
                     border="3px solid black"
@@ -191,7 +191,7 @@ const ProjectCard = () => {
                   </Flex>
 
                   <Flex
-                     h="22px"
+                    h="22px"
                     p={2}
                     boxShadow="4px 4px black"
                     border="3px solid black"
@@ -208,7 +208,7 @@ const ProjectCard = () => {
                 </SimpleGrid>
               </Flex>
 
-              <Text fontSize="lg" fontWeight={700}>
+              <Text fontSize="lg" fontWeight={600}>
                 聊天Go是一個任何人都可進入的公共聊天室，在聊天室中，您可以看到發訊息者的留言時間，
                 也能看到發訊息者的頭像。本作品主要練習使用firebase以及使用firebase提供的身份驗證方式進行登入。並練習使用Conditional
                 Rendering去渲染明亮模式/黑暗模式的顏色、以及有登入的用戶才可看到聊天室。
@@ -232,7 +232,7 @@ const ProjectCard = () => {
             </Flex>
           </Card>
 
-          <Card bg={cardBg1} h="100%" maxW="600px" color={color} border={border} boxShadow={boxShadow}>
+          <Card boxShadow={boxShadow} bg={cardBg1} h="100%" maxW="600px" color={color} border={border}>
             <Flex p={3} borderBottom="5px solid black" justify="space-between">
               <Text fontSize="xl" fontWeight={800}>
                 PROJECT 3
@@ -246,9 +246,9 @@ const ProjectCard = () => {
             <CardBody>
               <Flex direction="column">
                 <Text fontSize="4xl" fontWeight={800} mb={2}>
-                  Coin Tracker
+                  Shopping Cart
                 </Text>
-                <SimpleGrid columns={{ base: 3, md: 3, lg: 5 }} spacing={2}  mb={5}>
+                <SimpleGrid columns={{ base: 3, md: 3, lg: 5 }} spacing={2} mb={5}>
                   <Flex
                     h="22px"
                     p={2}
@@ -273,31 +273,64 @@ const ProjectCard = () => {
                     borderRadius="md"
                     color="white"
                     fontSize="xs"
+                    bg="gray.800"
+                    justify="center"
+                    align="center"
+                    fontWeight={600}
+                  >
+                    <Text>Next.js</Text>
+                  </Flex>
+
+                  <Flex
+                    h="22px"
+                    p={2}
+                    boxShadow="4px 4px black"
+                    border="3px solid black"
+                    borderRadius="md"
+                    color="white"
+                    fontSize="xs"
                     bg="blue.600"
                     justify="center"
                     align="center"
                     fontWeight={600}
                   >
-                    <Text>React</Text>
+                    <Text>Typescript</Text>
+                  </Flex>
+
+                  <Flex
+                    h="22px"
+                    p={2}
+                    boxShadow="4px 4px black"
+                    border="3px solid black"
+                    borderRadius="md"
+                    color="white"
+                    fontSize="xs"
+                    bg="purple.500"
+                    justify="center"
+                    align="center"
+                    fontWeight={600}
+                  >
+                    <Text>Redux</Text>
                   </Flex>
                 </SimpleGrid>
               </Flex>
 
-              <Text fontSize="lg" fontWeight={700}>
-                這是一個虛擬貨幣的即時百大排行榜，可以看到當前貨幣的最新資訊，包含名稱、價格、交易量，等等。本作品主要練習抓取API資料，並練習使用chart.js來呈現圖表。同時也練習react-router，熟悉跳頁方式的使用。另外也嘗試在撈資料時做Skeleton,提升使用者體驗。
+              <Text fontSize="lg" fontWeight={600}>
+                {" "}
+                簡潔的購物車網站。主要練習使用Next.js跟Typescript，並使用Redux-Toolkit執行狀態管理，同時也練習用Redux-Persist儲存資料，使用者重新整理後，購物車內容不會清空。
               </Text>
               {/* <Image src="/aboutme3.png" alt="" objectFit="contain" boxSize="250px" /> */}
             </CardBody>
 
             <Flex justify="center" gap={6}>
-              {/* <Button>查看內容</Button> */}
-              <Link href="https://coin-app-one.vercel.app/" target="_blank">
+              {/* <Button >查看內容</Button> */}
+              <Link href="https://next-chakra-shopping-cart.vercel.app/" target="_blank">
                 <Button bg={btnbg} _hover={{ bg: hoverBg }}>
                   前往連結
                 </Button>
               </Link>
-              <Link href="https://github.com/Lindahehexd/coin-app" target="_blank">
-                <Button gap={1} fontSize="lg" bg={btnbg} _hover={{ bg: hoverBg }}>
+              <Link href="https://github.com/Lindahehexd/next-chakra-shopping-cart" target="_blank">
+                <Button fontSize="lg" bg={btnbg} _hover={{ bg: hoverBg }}>
                   <Icon fontSize={30} as={AiFillGithub}></Icon>
                   Github
                 </Button>
@@ -319,9 +352,9 @@ const ProjectCard = () => {
             <CardBody>
               <Flex direction="column">
                 <Text fontSize="4xl" fontWeight={800} mb={2}>
-                  Todo List
+                  Coin Tracker
                 </Text>
-                <SimpleGrid columns={{ base: 3, md: 3, lg: 5 }} spacing={2}  mb={5}>
+                <SimpleGrid columns={{ base: 3, md: 3, lg: 5 }} spacing={2} mb={5}>
                   <Flex
                     h="22px"
                     p={2}
@@ -356,7 +389,80 @@ const ProjectCard = () => {
                 </SimpleGrid>
               </Flex>
 
-              <Text fontSize="lg" fontWeight={700}>
+              <Text fontSize="lg" fontWeight={600}>
+                這是一個虛擬貨幣的即時百大排行榜，可以看到當前貨幣的最新資訊，包含名稱、價格、交易量，等等。本作品主要練習抓取API資料，並練習使用chart.js來呈現圖表。同時也練習react-router，熟悉跳頁方式的使用。另外也嘗試在撈資料時做Skeleton,提升使用者體驗。
+              </Text>
+              {/* <Image src="/aboutme3.png" alt="" objectFit="contain" boxSize="250px" /> */}
+            </CardBody>
+
+            <Flex justify="center" gap={6}>
+              {/* <Button>查看內容</Button> */}
+              <Link href="https://coin-app-one.vercel.app/" target="_blank">
+                <Button bg={btnbg} _hover={{ bg: hoverBg }}>
+                  前往連結
+                </Button>
+              </Link>
+              <Link href="https://github.com/Lindahehexd/coin-app" target="_blank">
+                <Button gap={1} fontSize="lg" bg={btnbg} _hover={{ bg: hoverBg }}>
+                  <Icon fontSize={30} as={AiFillGithub}></Icon>
+                  Github
+                </Button>
+              </Link>
+            </Flex>
+          </Card>
+
+          <Card bg={cardBg1} h="100%" maxW="600px" color={color} border={border} boxShadow={boxShadow}>
+            <Flex p={3} borderBottom="5px solid black" justify="space-between">
+              <Text fontSize="xl" fontWeight={800}>
+                PROJECT 5
+              </Text>
+
+              <Flex>
+                <Icon as={FaRegArrowAltCircleRight} fontSize={30} />
+              </Flex>
+            </Flex>
+            {/* body */}
+            <CardBody>
+              <Flex direction="column">
+                <Text fontSize="4xl" fontWeight={800} mb={2}>
+                  Todo List
+                </Text>
+                <SimpleGrid columns={{ base: 3, md: 3, lg: 5 }} spacing={2} mb={5}>
+                  <Flex
+                    h="22px"
+                    p={2}
+                    boxShadow="4px 4px black"
+                    border="3px solid black"
+                    borderRadius="md"
+                    color="white"
+                    fontSize="xs"
+                    bg="teal.400"
+                    justify="center"
+                    align="center"
+                    fontWeight={600}
+                  >
+                    <Text>Chakra UI</Text>
+                  </Flex>
+
+                  <Flex
+                    h="22px"
+                    p={2}
+                    boxShadow="4px 4px black"
+                    border="3px solid black"
+                    borderRadius="md"
+                    color="white"
+                    fontSize="xs"
+                    bg="blue.600"
+                    justify="center"
+                    align="center"
+                    fontWeight={600}
+                  >
+                    <Text>React</Text>
+                  </Flex>
+                </SimpleGrid>
+              </Flex>
+
+              <Text fontSize="lg" fontWeight={600}>
                 此作品能讓使用者新增代辦事項及刪除完成事項，主要練習React,也同時熟悉Chakra UI的使用方式，並使用Local
                 Storage將資料存在本地空間。
               </Text>
@@ -381,10 +487,10 @@ const ProjectCard = () => {
             </Flex>
           </Card>
 
-          <Card bg={cardBg1} h="100%" maxW="600px" color={color} border={border} boxShadow={boxShadow}>
+          <Card bg={cardBg2} h="100%" maxW="600px" color={color} border={border} boxShadow={boxShadow}>
             <Flex p={3} borderBottom="5px solid black" justify="space-between">
               <Text fontSize="xl" fontWeight={800}>
-                PROJECT 5
+                PROJECT 6
               </Text>
 
               <Flex>
@@ -397,7 +503,7 @@ const ProjectCard = () => {
                 <Text fontSize="4xl" fontWeight={800} mb={2}>
                   碳帶紙捲計算器
                 </Text>
-                <SimpleGrid columns={{ base: 3, md: 3, lg: 5 }} spacing={2}  mb={5}>
+                <SimpleGrid columns={{ base: 3, md: 3, lg: 5 }} spacing={2} mb={5}>
                   <Flex
                     h="22px"
                     p={2}
@@ -432,11 +538,11 @@ const ProjectCard = () => {
                 </SimpleGrid>
               </Flex>
 
-              <Text fontSize="lg" fontWeight={700}>
+              <Text fontSize="lg" fontWeight={600}>
                 此作品是因應上一份公司的產品需求所開發的小應用程式，可以透過現有標籤紙捲/碳帶來了解該耗材可印刷的總長度及印刷張數，本作品主要練習基本的css以及react的prop傳遞。
               </Text>
 
-              <Text fontSize="lg" fontWeight={700}></Text>
+              <Text fontSize="lg" fontWeight={600}></Text>
               {/* <Image src="/aboutme3.png" alt="" objectFit="contain" boxSize="250px" /> */}
             </CardBody>
 
